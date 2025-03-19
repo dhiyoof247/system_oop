804207412044abe199db55ca3bd873d942ae5de6
dhiyoof247 <dhiyoofahammed247@gmail.com>
2025-03-19 14:49:09 +0200

    Create 19_march_2025_task1.cpp

---
+++ 19_march_2025_task1.cpp
@@ -0,0 +1,29 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+  int day;
+ cout<<"hey please write a number to print the day";
+ cin>>day;
+{ 
+    if(day==1){
+   cout<<"day is monday";
+   }else if (day==2){
+   cout<<"day is tuesday";
+   }else if(day==3){
+    cout<<"day is wensday";
+  }else if(day==4){
+    cout<<"day is thursday";
+    }else if(day==5){
+    cout<<"day is friday";
+   }else if(day==6){
+    cout<<"day is saturday";
+    }else if(day==7){
+    cout<<"day is sunday";
+    }
+    else{ 
+     cout<<"enter number is incorrect . please write a number from 1 to 7";
+    }
+}
+    return 0;
+}
